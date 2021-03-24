63246eb5a6dcf6f19608c208c28b0556b2a042bc
Rahul Kumar <rahul10.pu@gmail.com>
2021-03-24 20:28:58 +0530

    added getMin in stack

--- src/com/upgrad/fop/Stack.java
+++ src/com/upgrad/fop/Stack.java
@@ -5,6 +5,7 @@ class Stack
     private int arr[];
     private int top;
     private int capacity;
+    private int min;
 
     // Constructor to initialize the stack
     Stack(int size)
@@ -13,7 +14,16 @@ class Stack
         capacity = size;
         top = -1;
     }
-
+    int getMin(){
+        if (isEmpty())
+        {
+            System.out.println("Underflow\nProgram Terminated");
+            System.exit(1);
+        }else{
+            return min;
+        }
+        return -1;
+    }
     // Utility function to add an element `x` to the stack
     public void push(int x)
     {
@@ -22,9 +32,20 @@ class Stack
             System.out.println("Overflow\nProgram Terminated\n");
             System.exit(1);
         }
-
+        if (isEmpty())
+        {
+            min=x;
+            arr[++top] = x;
+           return;
+        }
         System.out.println("Inserting " + x);
-        arr[++top] = x;
+//        arr[++top] = x;
+        if (x<min){
+            arr[++top] = 2*x-min;
+            min=x;
+        }else{
+            arr[++top] = x;
+        }
     }
 
     // Utility function to pop a top element from the stack
@@ -40,14 +61,27 @@ class Stack
         System.out.println("Removing " + peek());
 
         // decrease stack size by 1 and (optionally) return the popped element
-        return arr[top--];
+//        return arr[top--];
+        if(arr[top]<min){
+            min=2*min-arr[top];
+            return arr[top--];
+        }else{
+            return arr[top--];
+        }
     }
 
     // Utility function to return the top element of the stack
     public int peek()
     {
+
+
         if (!isEmpty()) {
-            return arr[top];
+//            return arr[top];
+            if(arr[top]<min){
+                return  min;
+            }else{
+                return arr[top];
+            }
         }
         else {
             System.exit(1);
@@ -77,28 +111,45 @@ class Main
 {
     public static void main (String[] args)
     {
-        Stack stack = new Stack(3);
+        Stack stack = new Stack(10);
+        stack.push(5);
+        System.out.println(stack.getMin());
+        stack.push(10);
+        System.out.println(stack.getMin());
+        stack.push(4);
+        System.out.println(stack.peek());
+        System.out.println(stack.getMin());
+        stack.push(3);
+        System.out.println(stack.getMin());
+        stack.push(2);
+        System.out.println(stack.getMin());
+        stack.push(1);
+        System.out.println(stack.getMin());
+        stack.push(40);
+        System.out.println(stack.peek());
+        System.out.println(stack.getMin());
 
-        stack.push(1);      // inserting 1 in the stack
-        stack.push(2);      // inserting 2 in the stack
 
-        stack.pop();        // removing the top element (2)
-        stack.pop();        // removing the top element (1)
-
-        stack.push(3);      // inserting 3 in the stack
-
-        System.out.println("The top element is " + stack.peek());//peek is top
-        System.out.println("The stack size is " + stack.size());
-
-        stack.pop();        // removing the top element (3)
-
-        // check if the stack is empty
-        if (stack.isEmpty()) {
-            System.out.println("The stack is empty");
-        }
-        else {
-            System.out.println("The stack is not empty");
-        }
+//        stack.push(1);      // inserting 1 in the stack
+//        stack.push(2);      // inserting 2 in the stack
+//
+//        stack.pop();        // removing the top element (2)
+//        stack.pop();        // removing the top element (1)
+//
+//        stack.push(3);      // inserting 3 in the stack
+//
+//        System.out.println("The top element is " + stack.peek());//peek is top
+//        System.out.println("The stack size is " + stack.size());
+//
+//        stack.pop();        // removing the top element (3)
+//
+//        // check if the stack is empty
+//        if (stack.isEmpty()) {
+//            System.out.println("The stack is empty");
+//        }
+//        else {
+//            System.out.println("The stack is not empty");
+//        }
     }
 }
 
